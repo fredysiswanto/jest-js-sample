f005cb98dd5c87185f14e0cbf7fb1cd44913ff62
Fredy Siswanto <45227018+fredysiswanto@users.noreply.github.com>
2025-07-09 09:06:30 +0700

    clone repo from https://github.com/fredysiswanto/template-api-testing-use-jest

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BASE_URL: 'https://fakestoreapi.com', // Ganti dengan URL API asli
+  HEADERS: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+};
